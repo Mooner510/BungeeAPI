7f235dbd9c7e5575f09be03b67b1ea94f72dc6b7
Mooner510 <lmck510@gmail.com>
2022-08-27 00:45:48 +0900

    Changed Player DB Creation

--- src/main/java/org/mooner/moonerbungeeapi/db/ChatDB.java
+++ src/main/java/org/mooner/moonerbungeeapi/db/ChatDB.java
@@ -33,6 +33,7 @@ public class ChatDB {
                         "CREATE TABLE IF NOT EXISTS Player (" +
                                 "id INTEGER NOT NULL UNIQUE," +
                                 "uuid TEXT NOT NULL UNIQUE," +
+                                "name TEXT," +
                                 "PRIMARY KEY(id AUTOINCREMENT))")
         ) {
             s.execute();
